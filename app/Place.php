1c77f0941102f9f0287c20e30f905fb164da7b8e
dinanvn <dinanoviana99@gmail.com>
2020-08-25 17:59:01 +0700

    soft deleting implemented

--- app/Place.php
+++ app/Place.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Place extends Model
 {
+    use SoftDeletes;
+
     // Mass assignment with fillable = field yang boleh diisi
     // Mass assignment with guarded = field yang tidak boleh diisi
     protected $fillable = ['name', 'city', 'postalCode'];

8051058047f2cfac24330eee0cb24fa6c97218ea
dinanvn <dinanoviana99@gmail.com>
2020-08-25 16:37:57 +0700

    retrieving old input

--- resources/views/places/create.blade.php
+++ resources/views/places/create.blade.php
@@ -12,21 +12,21 @@
                 @csrf
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter cultural place's name" name="name">
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter cultural place's name" name="name" value="{{ old('name') }}">
                     @error('name')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="city">City</label>
-                    <input type="text" class="form-control @error('city') is-invalid @enderror" id="city" placeholder="Enter city" name="city">
+                    <input type="text" class="form-control @error('city') is-invalid @enderror" id="city" placeholder="Enter city" name="city" value="{{ old('city') }}">
                     @error('city')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="postalCode">Postal Code</label>
-                    <input type="text" class="form-control @error('postalCode') is-invalid @enderror" id="postalCode" placeholder="Enter postal code" name="postalCode">
+                    <input type="text" class="form-control @error('postalCode') is-invalid @enderror" id="postalCode" placeholder="Enter postal code" name="postalCode" value="{{ old('postalCode') }}">
                     @error('postalCode')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
